ce183a3a011dc252623b4cb9c5057d973a8ff6bf
Joel Hans <j@joelhans.com>
2026-05-17 18:45:04 -0700

    add split personality

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import remarkSmartypants from 'remark-smartypants';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -10,6 +11,9 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  markdown: {
+    remarkPlugins: [remarkSmartypants],
+  },
   integrations: [mdx(), sitemap(), icon()],
 
   vite: {
