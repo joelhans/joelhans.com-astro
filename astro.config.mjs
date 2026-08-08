135a0437fd1ac1a775757462eb98bb22f15b3000
Joel Hans <j@joelhans.com>
2026-08-08 05:33:39 -0700

    Fix site domain and add robots.txt for sitemap discovery

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://joelhans.com',
   markdown: {
     remarkPlugins: [remarkSmartypants],
   },
